ed4bfbc811ff662a2f91f064a12fa695347f1eca
akashgudadhe <akashgudadhe112@gmail.com>
2022-03-26 12:06:25 +0530

    create get all post method

--- src/main/java/com/springrest/springrestblog/service/PostService.java
+++ src/main/java/com/springrest/springrestblog/service/PostService.java
@@ -1,9 +1,12 @@
 package com.springrest.springrestblog.service;
 
+import java.util.List;
+
 import org.hibernate.event.spi.PostDeleteEvent;
 
 import com.springrest.springrestblog.payload.PostDto;
 
 public interface PostService {
   PostDto createPost(PostDto postDto);
+  List<PostDto> getAllPosts();
 }
