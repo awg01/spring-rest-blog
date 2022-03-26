c71eed4d487ac1df67743132d5b405471db1c0a3
akashgudadhe <akashgudadhe112@gmail.com>
2022-03-26 12:08:46 +0530

    created methods for dto and entity conversion

--- src/main/java/com/springrest/springrestblog/service/impl/PostServiceImpl.java
+++ src/main/java/com/springrest/springrestblog/service/impl/PostServiceImpl.java
@@ -12,37 +12,49 @@ import com.springrest.springrestblog.service.PostService;
 @Service
 public class PostServiceImpl implements PostService {
 	private PostRepository postRepository;
-	
-	
+
 	public PostServiceImpl(PostRepository postRepository) {
 		super();
 		this.postRepository = postRepository;
 	}
 
-
 	@Override
 	public PostDto createPost(PostDto postDto) {
-		//convert dto to entity
+		// convert dto to entity
+		Post post = mapToEntity(postDto);
+		
+		Post newPost = postRepository.save(post);
+
+		// convert entity to dto
+		PostDto postResponse = mapToDto(newPost);
+
+		return postResponse;
+	}
+
+	@Override
+	public List<PostDto> getAllPosts() {
+
+		return null;
+	}
+
+	// convert entity to dto
+	private PostDto mapToDto(Post post) {
+		PostDto postDto = new PostDto();
+		postDto.setId(post.getId());
+		postDto.setTitle(post.getTitle());
+		postDto.setDescription(post.getDescription());
+		postDto.setContent(post.getContent());
+
+		return postDto;
+	}
+
+	// convert dto to entity
+	private Post mapToEntity(PostDto postDto) {
 		Post post = new Post();
 		post.setTitle(postDto.getTitle());
 		post.setDescription(postDto.getDescription());
 		post.setContent(postDto.getContent());
-		
-		Post newPost = postRepository.save(post);
-		
-		//convert entity to dto
-		PostDto postResponse = new PostDto();
-		postResponse.setId(newPost.getId());
-		postResponse.setTitle(newPost.getTitle());
-		postResponse.setDescription(newPost.getDescription());
-		postResponse.setContent(newPost.getContent());
-		
-		return postResponse;
-	}
-	
-	@Override
-	public List<PostDto> getAllPosts() {
-		
-		return null;
+
+		return post;
 	}
 }
