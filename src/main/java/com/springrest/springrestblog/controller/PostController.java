e8bdc3949a4edd6e0496676f5777860b6aeceded
akashgudadhe <akashgudadhe112@gmail.com>
2022-03-26 12:06:48 +0530

    created mapping for getting all posts

--- src/main/java/com/springrest/springrestblog/controller/PostController.java
+++ src/main/java/com/springrest/springrestblog/controller/PostController.java
@@ -1,5 +1,30 @@
 package com.springrest.springrestblog.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.springrest.springrestblog.payload.PostDto;
+import com.springrest.springrestblog.service.PostService;
+
+@RestController
+@RequestMapping("/api/posts")
 public class PostController {
+
+	private PostService postService;
+
+	public PostController(PostService postService) {
+		super();
+		this.postService = postService;
+	}
+	
+	//create blog post
+	@PostMapping
+	public ResponseEntity<PostDto> createPost(@RequestBody PostDto postDto) {
+		return new ResponseEntity<>(postService.createPost(postDto), HttpStatus.CREATED);
+	}
 
 }
