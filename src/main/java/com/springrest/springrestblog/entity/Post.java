38b909132927bca69176288282841d79bfbac5f4
akashgudadhe <akashgudadhe112@gmail.com>
2022-03-29 22:58:16 +0530

    added test for createPost method

--- src/main/java/com/springrest/springrestblog/entity/Post.java
+++ src/main/java/com/springrest/springrestblog/entity/Post.java
@@ -7,12 +7,15 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
+
 
 @Entity
 public class Post {
---
+++ src/test/java/com/springrest/springrestblog/repository/PostRepositoryTests.java
@@ -0,0 +1,33 @@
+package com.springrest.springrestblog.repository;
+
+import javax.persistence.Column;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import com.springrest.springrestblog.entity.Post;
+
+@DataJpaTest //loads repository layer components - auto adds in memory db
+public class PostRepositoryTests {
+   @Autowired
+   private PostRepository postRepository;
+   
+   //junit test for createPost method
+   @DisplayName("Junit test for create post method")
+   @Test
+   public void givenPostObject_whenCreate_thenReturnCreatedPost() {
+	   //given - precondition
+	   Post post = Post.builder().title("first post").content("contents").description("test post create method").build();
+			   
+	   //when - action to test
+	   Post createdPost = postRepository.save(post);
+	   
+	   //then - verify the output
+	   assertThat(createdPost).isNotNull();
+	   assertThat(createdPost.getId()).isGreaterThan(0);
+	   
+   }
+}
