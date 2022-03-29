56f5ac5b4c9ce1d13166f7c72021e583d6e8e630
akashgudadhe <akashgudadhe112@gmail.com>
2022-03-29 23:28:35 +0530

    added @AutoConfigureTestDatabase on test class

--- src/test/java/com/springrest/springrestblog/repository/PostRepositoryTests.java
+++ src/test/java/com/springrest/springrestblog/repository/PostRepositoryTests.java
@@ -6,11 +6,13 @@ import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import com.springrest.springrestblog.entity.Post;
 
 @DataJpaTest //loads repository layer components - auto adds in memory db
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE) //Annotation that can be applied to a test class to configure a test database to use instead of the application-defined or auto-configured DataSource
 public class PostRepositoryTests {
    @Autowired
    private PostRepository postRepository;
